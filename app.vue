d982d702033a196527be364195cd4ccca136ce77
George Alexandrou <cysystemcy@gmail.com>
2022-05-03 12:51:17 +0300

    darkmode initiated

---
+++ app.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const { isDarkMode } = useDarkMode() ;
+</script>
+
+<template>
+  <div :style=" isDarkMode ? { backgroundColor: 'black', color:'white' } : null" >
+    <Nav />
+    <Heading />
+    <Cards />
+  </div>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  font-family: Georgia;
+}
+</style>
